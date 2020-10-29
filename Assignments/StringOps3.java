216d517ac1333ccefeb5d81ed4c709b4c763c030
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-29 12:53:18 +0530

    Add StringOps3.java

---
+++ Assignments/StringOps3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StringOps3 {
+    // Find the smallest string in a whitespace-separated string
+    static void smallestString(String s) {
+        String temp = "";
+        String[] words = s.split("\\s");
+        temp = words[0];
+        for (String str : words) {
+            if (str.length() < temp.length()) {
+                temp = str;
+            }
+        }
+        System.out.println("Smallest String: " + temp);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String s = sc.nextLine();
+        sc.close();
+        smallestString(s);
+    }
+
+}
